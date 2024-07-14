1d76a7d4705b6df7421900c16dfd3287c45fe853
ErrQD0303 <quocdat3396@gmail.com>
2024-07-15 00:11:07 +0700

    Adding TailwindCSS to the project and Style part of the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    //override the default fontFamily system of Tailwind
+    fontFamily: {
+      // Create a new font's class called font-sans
+      sans: "Roboto Mono, monospace",
+    },
+    //Extend the current Tailwind class
+    extend: {
+      // colors: {
+      //   pizza: "#123456",
+      // },
+      // This is just for fun
+      fontSize: {
+        huge: ["80rem", { lineHeight: "1" }],
+      },
+      // Important for mobile browser
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
